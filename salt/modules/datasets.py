b7b73ff8b71d281f8543886ae5ad8dfa405cbcfe
dreamflyer <gleb@onpositive.com>
2019-12-18 09:55:37 +0700

    project cleanup

--- salt/modules/datasets.py
+++ salt/modules/datasets.py
@@ -2,8 +2,8 @@ from musket_core import image_datasets,datasets
 
 @datasets.dataset_provider(origin="train.csv",kind="BinarySegmentationDataSet")
 def getTrain():
-    return image_datasets.BinarySegmentationDataSet(["test\images","train\images"],"train.csv","id","rle_mask")
+    return image_datasets.BinarySegmentationDataSet(["test/images","train/images"],"train.csv","id","rle_mask")
 
 @datasets.dataset_provider(origin="sample_submission.csv",kind="BinarySegmentationDataSet")
 def getTest():
-    return image_datasets.BinarySegmentationDataSet(["test\images","train\images"],"sample_submission.csv","id","rle_mask")
+    return image_datasets.BinarySegmentationDataSet(["test/images","train/images"],"sample_submission.csv","id","rle_mask")
