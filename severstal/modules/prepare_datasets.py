37cf6d9fda851d9357aff785588a8c92fa4657bb
Dmitry Karpenko <dmitry@onpositive.com>
2020-01-14 18:42:14 +0700

    Readme and cleanup - experiments renamed
    Change-Id: Ibe336bf35d90932bc00be62bef79bdb99102753b

--- severstal/modules/prepare_datasets.py
+++ severstal/modules/prepare_datasets.py
@@ -16,15 +16,7 @@ def prepare_ds():
         return
     print("Preparing classification dataset...")
     data_frame = pd.read_csv(os.path.join(ds_path, 'train.csv'))
-    #classes = 4
-    # Let's split our dataset into 4 datasets, one per each defect class
-    #frames = [data_frame[data_frame['ClassId'] == (i + 1)] for i in range(classes)]
-    # We already splitted dataset into 4 parts, one per each class, so we don't need classId any longer
-    #filtered_frames = [frame.drop('ClassId', axis=1) for frame in frames]
-    # Save classes into 4 independent csv files
-    #for frm, idx in zip(filtered_frames, range(classes)): 
-    #    frm.to_csv(os.path.join(ds_path, 'train' + str(idx + 1) + '.csv'), index=False)
-    
+        
     classify_frame = data_frame.drop('EncodedPixels', axis=1)
     existing = set(classify_frame['ImageId'].tolist())
     img_path = os.path.join(ds_path, 'train_images')
