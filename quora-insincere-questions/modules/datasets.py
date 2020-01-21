f8bbe0ff5255582183c54412e995d98c4b1df5a5
Dmitry Karpenko <dmitry@onpositive.com>
2020-01-21 18:12:44 +0700

    Fix dataset path for insincere questions example
    Change-Id: Iae2a887873ecc5b8510c7f6f366224d28e64d111

--- quora-insincere-questions/modules/datasets.py
+++ quora-insincere-questions/modules/datasets.py
@@ -3,9 +3,9 @@ from musket_core import datasets
 
 @datasets.dataset_provider(origin="train.csv",kind="TextClassificationDataSet")
 def get_sample():
-    return text_datasets.BinaryTextClassificationDataSet("quora-insincere-questions-classification/train.csv","question_text","target")
+    return text_datasets.BinaryTextClassificationDataSet("train.csv","question_text","target")
 
 @datasets.dataset_provider(origin="test.csv",kind="TextClassificationDataSet")
 def get_test():
-    return text_datasets.BinaryTextClassificationDataSet("quora-insincere-questions-classification/test.csv","question_text","target")
+    return text_datasets.BinaryTextClassificationDataSet("test.csv","question_text","target")
 
