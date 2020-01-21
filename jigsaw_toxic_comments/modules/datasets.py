37507a7765f181dc6e03342dbab68638f39a177a
Dmitry Karpenko <dmitry@onpositive.com>
2020-01-21 17:57:04 +0700

    Flatten dataset structure
    Change-Id: Ia6eb81a5e51c970af41655273bcaaf753ed32e25

--- jigsaw_toxic_comments/modules/datasets.py
+++ jigsaw_toxic_comments/modules/datasets.py
@@ -3,4 +3,4 @@ from musket_core import datasets
 
 @datasets.dataset_provider(origin="train.csv",kind="TextClassificationDataSet")
 def getTrain():
-    return text_datasets.MultiClassTextClassificationDataSet("train.csv/train.csv","comment_text","toxic|severe_toxic|obscene|threat|insult|identity_hate")
+    return text_datasets.MultiClassTextClassificationDataSet("train.csv","comment_text","toxic|severe_toxic|obscene|threat|insult|identity_hate")
