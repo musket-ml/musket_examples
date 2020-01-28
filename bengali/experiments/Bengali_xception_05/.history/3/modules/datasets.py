c55f1d062f9c1c9da13205d5f8703e3d68776338
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2020-01-29 01:09:48 +0700

    experiments with history

---
+++ bengali/experiments/Bengali_xception_05/.history/3/modules/datasets.py
@@ -0,0 +1,17 @@
+from musket_core import datasets,genericcsv
+from musket_core import image_datasets,datasets
+
+@datasets.dataset_provider(origin="train.csv",kind="GenericDataSet")
+def getBengali0():
+    return image_datasets.MultiOutputClassClassificationDataSet("bengali-cv19/train", "bengali-cv19/train.csv", 'image_id', ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic'])
+
+# def getBengali1():
+#     return image_datasets.MultiOutputClassClassificationDataSet("bengali/train_1", "bengali/train.csv", 'image_id', ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic'])
+
+@datasets.dataset_provider(origin="test.csv",kind="MultiClassificationDataset")
+def getBengaliTest0():
+    return image_datasets.MultiOutputClassClassificationDataSet("bengali/test_0", "bengali/test_flat.csv", 'image_id', ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic'])
+
+# @datasets.dataset_provider(origin="train.csv",kind="GenericDataSet")
+# def getBengali0_small():
+#     return image_datasets.MultiOutputClassClassificationDataSet("bengali/train_0", "bengali/train.csv", 'image_id', ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic'], len=10000)
--- bengali/utils/convert_bengali_parquet_train.py
+++ bengali/utils/convert_bengali_parquet_train.py
@@ -2,18 +2,21 @@ import numpy as np
 import pandas as pd
 import tqdm
 import imageio
+from musket_core import context
 
 HEIGHT = 137
 WIDTH = 236
 SIZE = 256
 
+p=context.get_current_project_data_path()
+
+
 for i in range(4):
-    df = pd.read_parquet(f"D:/datasets/bengaliai-cv19/train_image_data_{i}.parquet")
+    df = pd.read_parquet(f"{p}/bengaliai-cv19/train_image_data_{i}.parquet")
     data0 = df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
-    data1 = 255 - df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
+    
+    #data1 = 255 - df.iloc[:, 1:].values.reshape(-1, HEIGHT, WIDTH).astype(np.uint8)
     for idx in tqdm.tqdm(range(len(df))):
         name = df.iloc[idx, 0]
         im0 = data0[idx]
-        im1 = data1[idx]
-        imageio.imwrite(f"D:/datasets/bengaliai-cv19/train_0/{name}.jpg", im0)
-        imageio.imwrite(f"D:/datasets/bengaliai-cv19/train_1/{name}.jpg", im1)
+        imageio.imwrite(f"{p}/bengaliai-cv19/train/{name}.jpg", im0)        
