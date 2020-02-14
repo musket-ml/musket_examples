bb7fe9cf711f25d4ad14df936eaea208118f9898
Dmitry Karpenko <dmitry@onpositive.com>
2020-02-14 20:07:55 +0700

    Auto-predict finished non-predicted experiments
    Change-Id: I24445b8bc75b898db6a9fe7ce888622f38c0d7eb

--- question_pairs/modules/callbacks.py
+++ question_pairs/modules/callbacks.py
@@ -3,12 +3,19 @@
 '''
 from musket_core.fit_callbacks import after_fit
 from musket_core import generic
+from musket_core import projects
+from pathlib import Path
+import os
+
 
 @after_fit
-def make_predictions():
-    preds = generic.parse("pairs_bert").predictions('test')
-    preds.dump('predictions_bert.csv')
-    
+def make_predictions(): #We will make predictions for those experiments, which are already finished, but don't have the csv file with predictions
+    experiments = projects.Project(Path(__file__).parent.parent).experiments()
+    for exp in experiments:
+        res_file = os.path.join(exp.path, exp.name() + '_result.csv')
+        if exp.isCompleted() and not os.path.exists(res_file):
+            preds = generic.parse(exp.path).predictions('test')
+            preds.dump(res_file)
 
-if __name__ == '__main__':
+if __name__ == '__main__': #for debug only
     make_predictions()
